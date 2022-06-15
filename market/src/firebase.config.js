0519637733af58570d5e14fb3fce027d69d6cd38
iandrei250 <prtdace@gmail.com>
2022-06-15 14:17:28 +0200

    initial commit and pages with firebase setup

---
+++ market/src/firebase.config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAxj3pm7gmuM1J0KkGugaJJLzJnLrYbyVQ",
+  authDomain: "house-market-4402b.firebaseapp.com",
+  projectId: "house-market-4402b",
+  storageBucket: "house-market-4402b.appspot.com",
+  messagingSenderId: "1010628802219",
+  appId: "1:1010628802219:web:3c036c0b896c3aecdd29ee",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
