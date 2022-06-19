de167c8ef6963eb64698636ffec5e16bdd3ec998
iandrei250 <prtdace@gmail.com>
2022-06-19 14:26:12 +0200

    get single listing data

--- market/src/App.js
+++ market/src/App.js
@@ -8,6 +8,7 @@ import Offers from "./pages/Offers";
 import SignIn from "./pages/SignIn";
 import Category from "./pages/Category";
 import CreateListing from "./pages/CreateListing";
+import Listing from "./pages/Listing";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import PrivateRoute from "./components/PrivateRoute";
@@ -20,6 +21,10 @@ function App() {
           <Route path="/" element={<Explore />} />
           <Route path="/offers" element={<Offers />} />
           <Route path="/category/:categoryName" element={<Category />} />
+          <Route
+            path="/category/:categoryName/:listingId"
+            element={<Listing />}
+          />
           <Route path="/profile" element={<PrivateRoute />}>
             <Route path="/profile" element={<Profile />} />
           </Route>
